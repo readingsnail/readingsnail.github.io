474b413d9918c18248596e1b55a0d1f079c76708
Woosuk Park <bjh13579@gmail.com>
2025-07-10 14:44:44 +0900

    Some Change

--- app.mjs
+++ app.mjs
@@ -4,36 +4,32 @@ import { defineConfig, createNotesQuery } from "./.app/app-config.js";
 export default defineConfig({
     title: "책읽는달팽의 다락방",
     description: "소개와 여럿 이야기들",
-
+    lang: "ko",
     editThisNote: {
     url: "https://github.com/readingsnail/impressium/edit/{{branch}}/{{file}}"
     }
-
-    openInNewTab: true,
 
     createNotesQuery({
     tree: true,
     });
 
     sidebar: {
-    sections: [
+    links: [
       {
-        label: "글 모음",
-        groups: [
-          {
-            // A collapsible group "Posts", expanded by default
-            label: "글",
-            query: createNotesQuery(),
-          },
-          {
-            // A collapsible group "Archived", collapsed by default
-            label: "보관됨",
-            expanded: false,
-            query: createNotesQuery(),
-          },
-        ],
+        // The url of the website
+        url: "https://github.com/readingsnail/impressium",
+
+        // The visible label of the link
+        label: "GitHub",
+
+        // The icon name
+        // For a list of available icons, see https://lucide.dev/icons/
+        icon: "github",
+
+        // If the link should open in a new tab (optional)
+        // Default: true
+        openInNewTab: false,
       },
     ],
-  },
   },
 });
