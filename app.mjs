bd75a07e2a8d64fe042450914d84d09161ff70f7
Woosuk Park <bjh13579@gmail.com>
2025-07-13 14:34:11 +0900

    Some Change

--- app.mjs
+++ app.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     },
     tags: {
     map: {
-        dynamic-content": "dynamic content
+        "dynamic-content": "dynamic content",
     },
     },
     sidebar: {
